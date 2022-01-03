b4180b196def11c9df5c431c107cdecfb26c22ce
cumet04 <medalhkr@gmail.com>
2022-01-03 13:50:31 +0900

    カーソル位置の単語を取ってみる

--- src/extension.ts
+++ src/extension.ts
@@ -14,9 +14,10 @@ export function activate(context: vscode.ExtensionContext) {
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('sbox-vscode-extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from sbox_vscode-extension!');
+		const e = vscode.window.activeTextEditor
+		if(!e) return;
+		const word = e.document.getText(e.document.getWordRangeAtPosition(e.selection.start))
+		console.log(word)
 	});
 
 	context.subscriptions.push(disposable);
